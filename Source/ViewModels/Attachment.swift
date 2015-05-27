65e2019a8d9766ca7f7d947e91db198e9939412e
Christoffer Winterkvist <chris@zenangst.com>
2015-05-27 19:46:42 +0200

    Refactor Attachment
    Changed the order, `source` goes first and `type` is second. I just
    thought that looked nicer.
    
    I also introduce a protocol for URLStringConvertible so that you can
    initialise the `Attachment` with a string instead of an `NSURL`. It
    looks cleaner and works kind of in the same way.
    
    To access the URL you just do `Attachment.source.url`.
    
    What do you think @vadymmarkov ?

--- Source/ViewModels/Attachment.swift
+++ Source/ViewModels/Attachment.swift
@@ -7,10 +7,22 @@ public struct Attachment {
   }
 
   public var type: Type
-  public var source: NSURL
+  public var source: URLStringConvertible
 
-  public init(type: Type, source: NSURL) {
+  public init(_ source: URLStringConvertible, _ type: Type) {
     self.type = type
     self.source = source
   }
+}
+
+public protocol URLStringConvertible {
+  var url: NSURL { get }
+}
+
+extension String: URLStringConvertible {
+
+  public var url: NSURL {
+    return NSURL(string: self)!
+  }
+
 }
