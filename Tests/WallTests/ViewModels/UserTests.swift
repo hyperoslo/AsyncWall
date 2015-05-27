02749b4c02386e5a8d3c8b051ff77bef6ceda0d7
Christoffer Winterkvist <chris@zenangst.com>
2015-05-27 20:08:58 +0200

    Add testURLConvertable

--- Tests/WallTests/ViewModels/UserTests.swift
+++ Tests/WallTests/ViewModels/UserTests.swift
@@ -2,4 +2,16 @@ import Foundation
 import XCTest
 
 class UserTests: XCTestCase {
+
+  func testURLConvertable() {
+    let url = "https://avatars2.githubusercontent.com/u/1340892?v=3&s=200"
+    let name = "John Hyperseed"
+    let user = User(name: name, avatar: url)
+
+    XCTAssertEqual(user.name, name)
+    XCTAssertEqual(user.avatar!.string, url)
+    XCTAssertEqual(user.avatar!.url, NSURL(string: url)!)
+    XCTAssertTrue(user.avatar!.url.isKindOfClass(NSURL.classForCoder()))
+  }
+
 }
