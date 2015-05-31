19f66bfbbdfc37e9a65f23942f2790b32ffaf0d0
Vadym Markov <markov.vadym@gmail.com>
2015-05-31 21:21:59 +0200

    Change attachment default value

--- Source/Models/Post.swift
+++ Source/Models/Post.swift
@@ -11,11 +11,10 @@ public class Post: NSObject {
   public var parent: Post?
   public var comments = [Post]()
 
-  public init(text: String, date: NSDate, author: User? = nil, attachments: [Attachment]? = []) {
+  public init(text: String, date: NSDate, author: User? = nil, attachments: [Attachment]? = nil) {
     self.text = text
     self.date = date
     self.author = author
     self.attachments = attachments
   }
-
 }
