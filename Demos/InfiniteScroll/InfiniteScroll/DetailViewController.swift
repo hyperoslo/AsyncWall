4cf089178b651654520643c6ddeb6e39f87d59d5
Christoffer Winterkvist <chris@zenangst.com>
2015-05-31 23:30:40 +0200

    Use faker in detail view

--- Demos/InfiniteScroll/InfiniteScroll/DetailViewController.swift
+++ Demos/InfiniteScroll/InfiniteScroll/DetailViewController.swift
@@ -1,7 +1,10 @@
 import Foundation
 import Wall
+import Faker
 
 class DetailViewController: WallController {
+
+  let faker = Faker()
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -11,15 +14,17 @@ class DetailViewController: WallController {
     let delayTime = dispatch_time(DISPATCH_TIME_NOW,
       Int64(0.1 * Double(NSEC_PER_SEC)))
     dispatch_after(delayTime, dispatch_get_main_queue()) {
-      self.posts = self.generatePosts(1, to: 20)
+      self.posts = self.generatePosts(1, to: 3)
     }
   }
 
   func generatePosts(from: Int, to: Int) -> [Post] {
     var posts = [Post]()
     var startFrom = self.posts.count
+
     for i in from...to {
-      posts.append(Post(text: "Comment -> \(i+startFrom)", date: NSDate()))
+      let sencenceCount = Int(arc4random_uniform(8) + 1)
+      posts.append(Post(text: faker.lorem.sentences(amount: sencenceCount), date: NSDate()))
     }
     return posts
   }
