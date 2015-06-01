4a9b53586ccd4e9711d847ad3db514da2a982c5d
Christoffer Winterkvist <chris@zenangst.com>
2015-06-01 08:53:52 +0200

    Add more posts on initial load

--- Demos/InfiniteScroll/InfiniteScroll/ViewController.swift
+++ Demos/InfiniteScroll/InfiniteScroll/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: WallController, WallTapDelegate, WallScrollDelegate {
     let delayTime = dispatch_time(DISPATCH_TIME_NOW,
       Int64(1 * Double(NSEC_PER_SEC)))
     dispatch_after(delayTime, dispatch_get_main_queue()) {
-      self.posts = self.generatePosts(1, to: 20)
+      self.posts = self.generatePosts(1, to: 50)
     }
   }
 
