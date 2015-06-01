1b1c16f4e57415b7315deb19307e5b4bf1734f60
Christoffer Winterkvist <chris@zenangst.com>
2015-06-01 10:17:58 +0200

    Add custom initializer for CommentCellNode

--- Source/Nodes/CommentCellNode.swift
+++ Source/Nodes/CommentCellNode.swift
@@ -2,4 +2,10 @@ import UIKit
 import AsyncDisplayKit
 
 public class CommentCellNode: PostCellNode {
+
+    public override init(post: Post, width: CGFloat, _ delegate: AnyObject? = nil) {
+        super.init(post: post, width: width, delegate)
+
+        backgroundColor = UIColor(red:0.969, green:0.973, blue:0.976, alpha: 1)
+    }
 }
