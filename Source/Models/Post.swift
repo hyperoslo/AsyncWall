e411aa6b4dd50e4f9a83d840c6b3520048a5d635
Vadym Markov <markov.vadym@gmail.com>
2015-06-02 10:31:28 +0200

    Add location & group to post

--- Source/Models/Post.swift
+++ Source/Models/Post.swift
@@ -4,6 +4,8 @@ public class Post: NSObject {
 
   public var author: User?
   public var date: NSDate
+  public var group: String?
+  public var location: String?
   public var text: String?
   public var attachments: [Attachment]?
   public var likes: Int = 0
@@ -11,10 +13,13 @@ public class Post: NSObject {
   public var parent: Post?
   public var comments = [Post]()
 
-  public init(text: String, date: NSDate, author: User? = nil, attachments: [Attachment]? = nil) {
-    self.text = text
-    self.date = date
-    self.author = author
-    self.attachments = attachments
+  public init(text: String, date: NSDate, author: User? = nil,
+    group: String? = nil, location: String? = nil, attachments: [Attachment]? = nil) {
+      self.text = text
+      self.date = date
+      self.author = author
+      self.group = group
+      self.location = location
+      self.attachments = attachments
   }
 }
