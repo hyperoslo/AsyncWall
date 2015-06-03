9092f7c5416b74c6d5ab5a67457111707eab9875
Christoffer Winterkvist <chris@zenangst.com>
2015-06-03 19:36:02 +0200

    Add properties to Group

--- Source/Models/Group.swift
+++ Source/Models/Group.swift
@@ -2,4 +2,11 @@ import Foundation
 
 public struct Group {
 
+  public var name: String?
+  public var image: Image?
+
+  public init(name: String, image: Image? = nil) {
+    self.name = name
+    self.image = image
+  }
 }
