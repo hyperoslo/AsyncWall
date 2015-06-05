b9269b8cbd2026906bde43410a159ff9be9c18f8
Christoffer Winterkvist <chris@zenangst.com>
2015-06-05 09:25:50 +0200

    Add latitude and longitude to location

--- Source/Models/Location.swift
+++ Source/Models/Location.swift
@@ -1,9 +1,13 @@
 public struct Location {
 
   public var name: String
+  public var latitude: Float?
+  public var longitude: Float?
 
-  public init(name: String) {
+  public init(name: String, latitude: Float, longitude: Float) {
     self.name = name
+    self.latitude = latitude
+    self.longitude = longitude
   }
 
 }
