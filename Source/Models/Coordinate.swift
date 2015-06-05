571292d0b2033f926738202bf74783e6ca0c56e8
Christoffer Winterkvist <chris@zenangst.com>
2015-06-05 09:45:45 +0200

    Add Coordinate struct

---
+++ Source/Models/Coordinate.swift
@@ -0,0 +1,11 @@
+public struct Coordinate {
+
+  public let latitude: Double
+  public let longitude: Double
+
+  public init(latitude: Double, longitude: Double) {
+    self.latitude = latitude
+    self.longitude = longitude
+  }
+
+}
