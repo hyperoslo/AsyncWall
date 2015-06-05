8bbcab546fd63f7aa681612ea7e57ab6550deb91
Christoffer Winterkvist <chris@zenangst.com>
2015-06-05 09:58:13 +0200

    Implement LocationSpec

--- Tests/WallTests/Models/LocationSpec.swift
+++ Tests/WallTests/Models/LocationSpec.swift
@@ -4,6 +4,41 @@ import Nimble
 class LocationSpec: QuickSpec {
 
   override func spec() {
-    
+    describe("Location") {
+      let name = "Greece"
+      let coordinate = Coordinate(latitude: 3.14, longitude: 3.14)
+
+      var location : Location!
+
+      beforeEach {
+        location = Location(name: name, coordinate: coordinate)
+      }
+
+      describe("#init") {
+        context("with location") {
+          it("sets a name") {
+            expect(location.name).to(equal(name))
+          }
+
+          it ("has a coordinate") {
+            expect(location.coordinate).notTo(beNil())
+          }
+        }
+
+        context("without location") {
+          beforeEach {
+            location = Location(name: name, coordinate: nil)
+          }
+
+          it("sets a name") {
+            expect(location.name).to(equal(name))
+          }
+
+          it ("has a coordinate") {
+            expect(location.coordinate).to(beNil())
+          }
+        }
+      }
+    }
   }
 }
