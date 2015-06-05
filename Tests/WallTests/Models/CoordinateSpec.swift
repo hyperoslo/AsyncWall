9a9711feaae16ca65d8c0b012641c5d8003008fd
Christoffer Winterkvist <chris@zenangst.com>
2015-06-05 10:07:32 +0200

    Implement CoordinateSpec

--- Tests/WallTests/Models/CoordinateSpec.swift
+++ Tests/WallTests/Models/CoordinateSpec.swift
@@ -2,4 +2,27 @@ import Quick
 import Nimble
 
 class CoordinateSpec: QuickSpec {
+
+  override func spec() {
+    describe("Coordinate") {
+      let latitude = 3.14
+      let longitude = 3.24
+
+      var coordinate: Coordinate!
+
+      beforeEach {
+        coordinate = Coordinate(latitude: latitude, longitude: longitude)
+      }
+
+      describe("#init") {
+        context("with latitude and longitude") {
+          it ("has the right location") {
+            expect(coordinate.latitude).to(equal(latitude))
+            expect(coordinate.longitude).to(equal(longitude))
+          }
+        }
+      }
+    }
+  }
+
 }
