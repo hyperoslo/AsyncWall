4cced080cce3b04697be74662a5080cf08b1f4ad
Vadym Markov <markov.vadym@gmail.com>
2015-06-11 12:27:32 +0200

    Remove static vars

--- Source/Config.swift
+++ Source/Config.swift
@@ -4,20 +4,23 @@ import Haneke
 import TimeAgo
 
 public struct Config {
+  public var cache = Cache()
+  public var wall = Wall()
 
-  private static var dateFormatter: NSDateFormatter = {
+  private var dateFormatter: NSDateFormatter = {
     let dateFormatter = NSDateFormatter()
     dateFormatter.dateFormat = "MM-dd"
     return dateFormatter
     }()
 
   public struct Cache {
+    public var thumbnails = Thumbnails()
 
     public struct Thumbnails {
-      public static var format = "wall-thumbnails"
-      public static var storage: Haneke.Cache<UIImage> {
+      public var format = "wall-thumbnails"
+      public var storage: Haneke.Cache<UIImage> {
         let cache = Shared.imageCache
-        let format = Format<UIImage>(name: Cache.Thumbnails.format, diskCapacity: 20 * 1024 * 1024)
+        let format = Format<UIImage>(name: self.format, diskCapacity: 20 * 1024 * 1024)
         cache.addFormat(format)
 
         return cache
@@ -26,77 +29,94 @@ public struct Config {
   }
 
   public struct Wall {
-    public static var padding: CGFloat = 10
+    public var padding: CGFloat = 10
 
-    public static var thumbnailForAttachment: (attachment: Attachment, size: CGSize) -> URLStringConvertible = {
+    public var thumbnailForAttachment: (attachment: Attachment, size: CGSize) -> URLStringConvertible = {
       (attachment: Attachment, size: CGSize) -> URLStringConvertible in
       return attachment.thumbnail
     }
 
-    public static var stringFromPostDate: (date: NSDate) -> String = {
+    public var stringFromPostDate: (date: NSDate) -> String = {
       (date: NSDate) -> String in
       return date.timeAgo
     }
 
+    public var post = Post()
+    public var comment = Comment()
+
     public struct Post {
-      public static var backgroundColor = UIColor.whiteColor()
+      public var backgroundColor = UIColor.whiteColor()
+      public var header = Header()
+      public var attachments = Attachments()
+      public var text = Text()
+      public var footer = Footer()
+      public var actionBar = ActionBar()
+      public var control = Control()
+      public var divider = Divider()
 
       public struct Header {
-        public static var enabled = true
-        public static var height: CGFloat = 40
+        public var enabled = true
+        public var height: CGFloat = 40
+        public var author = Author()
+        public var group = Group()
+        public var location = Location()
+        public var date = Date()
 
         public struct Author {
-          public static var enabled = true
-          public static var horizontalPadding: CGFloat = 5
-          public static var verticalPadding: CGFloat = 1
-          public static var textAttributes = [
+          public var enabled = true
+          public var horizontalPadding: CGFloat = 5
+          public var verticalPadding: CGFloat = 1
+          public var textAttributes = [
             NSFontAttributeName: UIFont.boldSystemFontOfSize(14),
             NSForegroundColorAttributeName: UIColor.blackColor()
           ]
+          public var avatar = Avatar()
 
           public struct Avatar {
-            public static var enabled = true
-            public static var size: CGFloat = 32
-            public static var rounded = true
-            public static var placeholderColor = UIColor.lightGrayColor()
+            public var enabled = true
+            public var size: CGFloat = 32
+            public var rounded = true
+            public var placeholderColor = UIColor.lightGrayColor()
           }
         }
 
         public struct Group {
-          public static var enabled = true
-          public static var textAttributes = [
+          public var enabled = true
+          public var textAttributes = [
             NSFontAttributeName: UIFont.boldSystemFontOfSize(12),
             NSForegroundColorAttributeName: UIColor.blackColor()
           ]
+          public var divider = Divider()
 
           public struct Divider {
-            public static var enabled = true
-            public static var textAttributes = [
+            public var enabled = true
+            public var textAttributes = [
               NSFontAttributeName: UIFont.systemFontOfSize(12),
               NSForegroundColorAttributeName: UIColor.grayColor()
             ]
-            public static var text = ">"
+            public var text = ">"
           }
         }
 
         public struct Location {
-          public static var enabled = true
-          public static var textAttributes = [
+          public var enabled = true
+          public var textAttributes = [
             NSFontAttributeName: UIFont.systemFontOfSize(12),
             NSForegroundColorAttributeName: UIColor.grayColor()
           ]
+          public var icon = Icon()
 
           public struct Icon {
-            public static var enabled = true
-            public static var padding: CGFloat = 3
-            public static var size: CGFloat = 12
-            public static var image: UIImage?
+            public var enabled = true
+            public var padding: CGFloat = 3
+            public var size: CGFloat = 12
+            public var image: UIImage?
           }
         }
 
         public struct Date {
-          public static var enabled = true
-          public static var textAttributes = [
+          public var enabled = true
+          public var textAttributes = [
             NSFontAttributeName: UIFont.systemFontOfSize(12),
             NSForegroundColorAttributeName: UIColor.grayColor()
           ]
@@ -104,15 +124,16 @@ public struct Config {
       }
 
       public struct Attachments {
-        public static var ratio: CGFloat = 3 / 2
-        public static var padding: CGFloat = 10
+        public var ratio: CGFloat = 3 / 2
+        public var padding: CGFloat = 10
+        public var counter = Counter()
 
         public struct Counter {
-          public static var enabled = true
-          public static var padding: CGFloat = 3
-          public static var boxSize = CGSize(width: 40.0, height: 30.0)
-          public static var boxGap: CGFloat = 3
-          public static var textAttributes = [
+          public var enabled = true
+          public var padding: CGFloat = 3
+          public var boxSize = CGSize(width: 40.0, height: 30.0)
+          public var boxGap: CGFloat = 3
+          public var textAttributes = [
             NSFontAttributeName: UIFont.systemFontOfSize(14),
             NSForegroundColorAttributeName: UIColor.grayColor()
           ]
@@ -120,37 +141,40 @@ public struct Config {
       }
 
       public struct Text {
-        public static var textAttributes = [
+        public var textAttributes = [
           NSFontAttributeName: UIFont.systemFontOfSize(14),
           NSForegroundColorAttributeName: UIColor.grayColor()
         ]
       }
 
       public struct Footer {
-        public static var enabled = true
-        public static var height: CGFloat = 40
-        public static var horizontalPadding: CGFloat = 10
+        public var enabled = true
+        public var height: CGFloat = 40
+        public var horizontalPadding: CGFloat = 10
+        public var likes = Likes()
+        public var comments = Comments()
+        public var seen = Seen()
 
         public struct Likes {
-          public static var enabled = true
-          public static var textAttributes = [
+          public var enabled = true
+          public var textAttributes = [
             NSFontAttributeName: UIFont.boldSystemFontOfSize(14),
             NSForegroundColorAttributeName: UIColor.blackColor()
           ]
         }
 
         public struct Comments {
-          public static var enabled = true
-          public static var textAttributes = [
+          public var enabled = true
+          public var textAttributes = [
             NSFontAttributeName: UIFont.boldSystemFontOfSize(14),
             NSForegroundColorAttributeName: UIColor.blackColor()
           ]
         }
 
         public struct Seen {
-          public static var enabled = true
-          public static var text = NSLocalizedString("Seen by", comment: "")
-          public static var textAttributes = [
+          public var enabled = true
+          public var text = NSLocalizedString("Seen by", comment: "")
+          public var textAttributes = [
             NSFontAttributeName: UIFont.italicSystemFontOfSize(12),
             NSForegroundColorAttributeName: UIColor.grayColor()
           ]
@@ -158,26 +182,28 @@ public struct Config {
       }
 
       public struct ActionBar {
-        public static var enabled = true
-        public static var dividerEnabled = true
-        public static var height: CGFloat = 40
-        public static var backgroundColor = UIColor.clearColor()
+        public var enabled = true
+        public var dividerEnabled = true
+        public var height: CGFloat = 40
+        public var backgroundColor = UIColor.clearColor()
+        public var likeButton = LikeButton()
+        public var commentButton = CommentButton()
 
         public struct LikeButton {
-          public static var enabled = true
-          public static var title: String? = NSLocalizedString("Like", comment: "")
-          public static var image: UIImage?
-          public static var textAttributes = [
+          public var enabled = true
+          public var title: String? = NSLocalizedString("Like", comment: "")
+          public var image: UIImage?
+          public var textAttributes = [
             NSFontAttributeName: UIFont.boldSystemFontOfSize(14),
             NSForegroundColorAttributeName: UIColor.lightGrayColor()
           ]
         }
 
         public struct CommentButton {
-          public static var enabled = true
-          public static var title: String? = NSLocalizedString("Comment", comment: "")
-          public static var image: UIImage?
-          public static var textAttributes = [
+          public var enabled = true
+          public var title: String? = NSLocalizedString("Comment", comment: "")
+          public var image: UIImage?
+          public var textAttributes = [
             NSFontAttributeName: UIFont.boldSystemFontOfSize(14),
             NSForegroundColorAttributeName: UIColor.lightGrayColor()
           ]
@@ -185,24 +211,25 @@ public struct Config {
       }
 
       public struct Control {
-        public static var size = CGSize(width: 100.0, height: 35.0)
-        public static var padding: CGFloat = 5
-        public static var imageSize = CGSize(width: 22, height: 22)
+        public var size = CGSize(width: 100.0, height: 35.0)
+        public var padding: CGFloat = 5
+        public var imageSize = CGSize(width: 22, height: 22)
       }
 
       public struct Divider {
-        public static var enabled = true
-        public static var height: CGFloat = 1
-        public static var backgroundColor = UIColor.lightGrayColor()
+        public var enabled = true
+        public var height: CGFloat = 1
+        public var backgroundColor = UIColor.lightGrayColor()
       }
     }
 
     public struct Comment {
-      public static var backgroundColor =  UIColor(red:0.969, green:0.973, blue:0.976, alpha: 1)
+      public var backgroundColor =  UIColor(red:0.969, green:0.973, blue:0.976, alpha: 1)
+      public var divider = Divider()
 
       public struct Divider {
-        public static var enabled = true
-        public static var backgroundColor = UIColor(red:0.925, green:0.933, blue:0.941, alpha: 1)
+        public var enabled = true
+        public var backgroundColor = UIColor(red:0.925, green:0.933, blue:0.941, alpha: 1)
       }
     }
   }
