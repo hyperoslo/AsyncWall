cc390649de3b8f513a8535448d8660491614bb41
Christoffer Winterkvist <chris@zenangst.com>
2015-07-13 20:46:33 +0200

    Add source to Image

--- Source/Models/Image.swift
+++ Source/Models/Image.swift
@@ -1,10 +1,12 @@
 public class Image: Attachment {
 
   public var thumbnail: URLStringConvertible?
+  public var source: URLStringConvertible?
 
   public init() {}
 
-  public init(_ thumbnail: URLStringConvertible?) {
+  public init(_ source: URLStringConvertible, thumbnail: URLStringConvertible? = nil) {
+    self.source = source
     self.thumbnail = thumbnail
   }
 }
