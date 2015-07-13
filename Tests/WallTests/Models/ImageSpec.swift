cdbd9f0e4e0c8934a9ca2f9bdaad146f6905305b
Christoffer Winterkvist <chris@zenangst.com>
2015-07-13 20:46:37 +0200

    Refactor tests

--- Tests/WallTests/Models/ImageSpec.swift
+++ Tests/WallTests/Models/ImageSpec.swift
@@ -7,6 +7,7 @@ class ImageSpec: QuickSpec {
   override func spec() {
     describe("Image") {
       let source = Faker().internet.url()
+      let thumbnail = Faker().internet.url()
       var image: Image!
 
       beforeEach {
@@ -15,12 +16,22 @@ class ImageSpec: QuickSpec {
 
       describe("#thumbnail") {
         it("has the correct source") {
-          expect(image.thumbnail?.string).to(equal(source))
+          expect(image.source?.string).to(equal(source))
+
+        }
+
+        it("does not have a thumbnail") {
+          expect(image.thumbnail).to(beNil())
+        }
+
+        it("has thumbnail") {
+          image.thumbnail = thumbnail
+          expect(image.thumbnail?.string).to(equal(thumbnail))
         }
 
         it("has the correct source URL") {
-          expect(image.thumbnail?.url).to(equal(source.url))
-          expect(image.thumbnail?.url).to(beAKindOf(NSURL.classForCoder()))
+          expect(image.source?.url).to(equal(source.url))
+          expect(image.source?.url).to(beAKindOf(NSURL.classForCoder()))
         }
       }
     }
