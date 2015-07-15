1b34a3e2238cb71fee00984f95235879ef350da4
Vadym Markov <markov.vadym@gmail.com>
2015-07-15 15:51:15 +0200

    Fix App.net demo

---
+++ Example/App.Net/App.Net/Post.swift
@@ -0,0 +1,25 @@
+import Wall
+import Sugar
+
+class Post: Postable {
+
+  var id = 0
+  var publishDate: NSDate?
+  var text: String?
+  var liked = false
+  var seen = false
+  var likeCount = 0
+  var seenCount = 0
+  var commentCount = 0
+  var author: Profileable?
+  var group: Groupable?
+  var location: Location?
+  var parent: Postable?
+  var attachments = [Attachable]()
+  var comments = [Postable]()
+
+  init(text: String? = nil, date: NSDate? = nil) {
+    self.text = text
+    self.publishDate = date
+  }
+}
